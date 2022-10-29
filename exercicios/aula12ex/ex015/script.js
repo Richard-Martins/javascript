f0a4d69dc4a6847a1492ebd6aea88aef5b266b2f
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-10-28 22:46:08 -0300

    att

---
+++ exercicios/aula12ex/ex015/script.js
@@ -0,0 +1,15 @@
+function verificar() {
+  var data = new Date();
+  var ano = data.getFullYear();
+  var fano = document.getElementById("txtano");
+  var res = document.querySelector("#res");
+  if (fano.value.length == 0 || fano.value > ano) {
+    window.alert("verifique os dados e tente novamente");
+  } else {
+    var fsex = document.getElementsByClassName("radsex");
+    var idade = ano - Number(fano.value);
+    var genero = "";
+    if (fsex[0].checked) {
+    }
+  }
+}
