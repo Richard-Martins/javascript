0056f9802427cfa5984a45f0a24b74f4d3f5506a
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-12-13 14:44:42 -0300

    remove

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -1,18 +1,48 @@
 function verificar() {
-  var data = new Date();
-  var ano = data.getFullYear();
-  var fano = document.getElementById("txtano");
-  var res = document.querySelector("#res");
+  let data = new Date();
+  let ano = data.getFullYear();
+  let fano = document.getElementById("txtano");
+  let res = document.getElementById("res");
   if (fano.value.length == 0 || fano.value > ano) {
-    window.alert("verifique os dados e tente novamente");
-  } else {
-    var fsex = document.getElementsByClassName("radsex");
-    var idade = ano - Number(fano.value);
-    var genero = "";
-    if (fsex[0].checked) {
-      genero = "Homem";
-    } else if (fsex[1].checked) {
-      genero = "Mulher";
+    alert("Digite um ano válido!");
+  }
+  let idade = ano - Number(fano.value);
+  let fsex = document.getElementsByName("radsex");
+  let genero = "";
+  let img = document.createElement("img");
+  img.setAttribute("id", "foto");
+  if (fsex[0].checked) {
+    genero = "Homem";
+    if (idade >= 0 && idade < 10) {
+      // Criança
+      img.setAttribute("src", "menino-criança.jpg");
+    } else if (idade < 21) {
+      // Jovem
+      img.setAttribute("src", "menino-adolescente.jpg");
+    } else if (idade < 50) {
+      // Adulto
+      img.setAttribute("src", "menino-adulto.jpg");
+    } else {
+      // Idoso
+      img.setAttribute("src", "menino-idoso.jpg");
+    }
+  } else if (fsex[1].checked) {
+    genero = "Mulher";
+    if (idade >= 0 && idade < 10) {
+      // Criança
+      img.setAttribute("src", "menina-criança.jpg");
+    } else if (idade < 21) {
+      // Jovem
+      img.setAttribute("src", "menina-adolescente.jpg");
+    } else if (idade < 50) {
+      // Adulta
+      img.setAttribute("src", "menina-adulta.jpg");
+    } else {
+      // Idosa
+      img.setAttribute("src", "menina-idosa.jpg");
     }
   }
+  res.style.textAlign = "center";
+  res.innerHTML = `Seu gênero é ${genero} e você tem ${idade} anos`;
+  res.appendChild(img);
 }
